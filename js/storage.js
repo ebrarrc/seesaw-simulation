667f93a821d5c5d6ae8b22d4a3df2f0e3f6cbfc1
Ebrar Nur <ebrarnurr02@gmail.com>
2026-02-01 22:07:08 +0300

    storage structure has been created

---
+++ js/storage.js
@@ -0,0 +1,45 @@
+import { ballFallArea , seesawPlank , pivot} from "./globals.js";
+import { createBall } from "./ball.js";
+
+export function saveStorage(upcomingWeight){
+    const balls = document.querySelectorAll(".ball");
+
+    const data = Array.from(balls).map(ball => ({
+        offset : Number(ball.dataset.offsetX),
+        weight : Number(ball.dataset.weight)
+    }))
+
+
+    localStorage.setItem("seesawBalls" , JSON.stringify(data));
+    localStorage.setItem("upcomingWeight" , upcomingWeight);
+}
+
+export function getStorage(upcomingWeight){
+    const saved = JSON.parse(localStorage.getItem("seesawBalls") || "[]");
+    const savedUpcomingWeight = localStorage.getItem("upcomingWeight");
+
+    if(savedUpcomingWeight){
+        upcomingWeight = Number(savedUpcomingWeight);
+        nextWeight.textContent = upcomingWeight + "kg";
+    }
+
+    const containerArea = ballFallArea.getBoundingClientRect();
+    const seesawPlankArea = seesawPlank.getBoundingClientRect();
+    const seesawPlankTopPoint = seesawPlankArea.top - 10;
+
+    const pivotArea = pivot.getBoundingClientRect();
+    const pivotCenterX = pivotArea.left + pivotArea.width / 2;
+
+    saved.forEach(element => {
+        const clickLocX = (pivotCenterX + element.offset) - containerArea.left;
+
+        const newBall = createBall(clickLocX, seesawPlankTopPoint , true , item.weight);
+
+        newBall.dataset.offsetX = item.offset;
+        newBall.dataset.weight = item.weight;
+    });
+}
+
+
+
+getStorage();
