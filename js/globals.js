d0439782fe24c4753224f88d5d38f172a18e9aa0
Ebrar Nur <ebrarnurr02@gmail.com>
2026-02-01 21:06:05 +0300

    global elements that are expected to be used have been added

---
+++ js/globals.js
@@ -0,0 +1,12 @@
+export const ballFallArea = document.querySelector(".ball-fall-area");
+
+export const colors = {
+  1: '#8A244B', 2: '#9BC264', 3: '#982598',
+  4: '#FF7F11', 5: '#FF5FCF', 6: '#161E54',
+  7: '#0D4715', 8: '#0097a7', 9: '#393D7E', 10: '#D34E4E'
+}
+
+export const sizes = {
+  1: '20px', 2: '25px', 3: '30px', 4: '35px', 5: '40px',
+  6: '45px', 7: '50px', 8: '55px', 9: '60px', 10: '65px'
+}
